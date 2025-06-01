f912be15edc84ad288693533e4319a14955eb917
guyang <yangu@zju.edu.cn>
2025-06-01 17:01:56 +0800

    Update RLAEGA.m

--- RLAEGA.m
+++ RLAEGA.m
@@ -1,3 +1,3 @@
-The source code for the proposed RLAEGA will be made available upon request under review stage.
+The source code for the proposed RLAEGA will be made available upon request during the review stage
 Please contact the corresponding author at yanggu[at]zju.edu.cn.
 The code will be released publicly after the manuscript is accepted.
